2d9b7ff8c50fc7837e3845f39b77f8b4a1be4cfd
Simran0501 <simrangarg0501@gmail.com>
2021-05-09 20:59:13 +0530

    Added Website

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql2");
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "root",
+  port:"3306"
 });
 
 db.connect(function (err) {
